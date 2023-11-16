0e6538ca613cc06b0ff155f11c5f5bdfc3c748af
Ray Xia <xia.t@wustl.edu>
2023-11-16 15:57:44 -0600

    black & white stars withthe  swirly thing

--- sketch.js
+++ sketch.js
@@ -1,56 +1,100 @@
-let shapes = [];
-let numShapes = 30;
+var weather;
+var api = "https://api.openweathermap.org/data/2.5/weather?";
+var units = "&units=imperial";
+var APIkey = "&appid=ca7caccfa515826d1bc1bc37590f135b";
+
+var stars = [];
+var speed = 2;
+
+//api var
+
+var temp;
+var speed;
+var humidity;
+var pressure;
+var modPressure;
+
+var input; // Declare input variable
+let t = 0;
 
 function setup() {
-  createCanvas(800, 600);
-  noStroke();
+  createCanvas(windowWidth, windowHeight);
   
-  // Initialize shapes
-  for (let i = 0; i < numShapes; i++) {
-    let x = random(width);
-    let y = random(height);
-    let size = random(20, 50);
-    let shapeType = random() > 0.5 ? 'square' : 'circle';
-    let velocity = createVector(random(-1, 1), random(-1, 1)).mult(random(1, 3));
-    shapes.push(new Shape(x, y, size, shapeType, velocity));
+  //create starts
+  for (var i = 0; i < 300; i++) {
+    stars[i] = new Star();
   }
+
+  // Create an input element and position it at the bottom of the canvas
+  input = createInput();
+  input.position(20, height + 20);
+
+  // Create a button to trigger the weatherAsk function
+  var button = createButton("Input Location");
+  button.position(input.x + input.width + 10, height + 20);
+  button.mousePressed(weatherAsk);
+
+  // Call the weatherAsk function initially with the default input value
+  // weatherAsk();
+}
+
+function weatherAsk() {
+  var url = api + "q=" + input.value() + APIkey + units;
+  loadJSON(url, gotData);
+}
+
+function gotData(data) {
+  // also set variables
+  console.log(data);
+  weather = data;
+
+  // establish global variables
+  temp = weather.main.temp;
+  feelsLike = weather.main.feels_like;
+  speed = weather.wind.speed;
+  humidity = weather.main.humidity;
+  clouds = weather.clouds.all;
+  pressure = weather.main.pressure;
+  modPressure = (pow((pressure - 900), 3))
 }
 
 function draw() {
-  background(240);
+  background("#fff");
+  translate(width / 2, height / 2);
+  stroke("#0f0f0f");
+  strokeWeight(1);
+
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
   
-  for (let i = 0; i < shapes.length; i++) {
-    shapes[i].update();
-    shapes[i].display();
+  if (weather) {
+    console.log(pressure)
+    // translate(width / 2, height / 2);
+    stroke("#0f0f0f");
+    strokeWeight(1);
+   
+
+    for (let i = 0; i < humidity; i++) {
+      line(x1(t + i), y1(t + i), x2(t + i) + feelsLike, y2(t + i) + feelsLike);
+    }
+    t += 0.05 * speed;
   }
 }
 
-class Shape {
-  constructor(x, y, size, type, velocity) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.type = type; // 'square' or 'circle'
-    this.velocity = velocity;
-  }
+function x1(t) {
+  return sin(t / 10) * (pressure - 950);
+}
 
-  update() {
-    this.x += this.velocity.x;
-    this.y += this.velocity.y;
-    
-    if (this.x < -this.size || this.x > width + this.size || this.y < -this.size || this.y > height + this.size) {
-      this.x = random(width);
-      this.y = random(height);
-    }
-  }
+function y1(t) {
+  return cos(t / 10) * (pressure - 950);
+}
 
-  display() {
-    fill(0, 100);
-    if (this.type === 'square') {
-      rectMode(CENTER);
-      rect(this.x, this.y, this.size, this.size);
-    } else if (this.type === 'circle') {
-      ellipse(this.x, this.y, this.size, this.size);
-    }
-  }
+function x2(t) {
+  return sin(t / 15) * modPressure + sin(t / 25) * modPressure + sin(t / 35) * modPressure;
+}
+
+function y2(t) {
+  return cos(t / 15) * modPressure + cos(t / 25) * modPressure + cos(t / 35) * modPressure;
 }
